14a2929854498c12de4427a8d558528d71865268
Changzeng <1207338829@qq.com>
2018-04-14 20:18:55 +0800

    fix bug in mp3_to_wav.py

--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -1,20 +1,18 @@
 # encoding: utf-8
 
 import os
-import sys
 from pydub import AudioSegment
 
 mp3_path = "music/mp3/"
 wav_path = "music/wav/"
 
 for _file in os.listdir(mp3_path):
+	wav_name = _file.replace(".mp3", ".wav")
 	if _file == ".DS_Store":
 		continue
 	if os.path.exists(wav_path+wav_name):
 		continue
-	msg = "processing %s\r"+" "*100 % _file
-	sys.stdout.write(msg)
-	sys.stdout.flush()
-	wav_name = _file.replace(".mp3", ".wav")
+	msg = "processing %s\r" % _file
+	print(msg)
 	sound = AudioSegment.from_mp3(mp3_path+_file)
 	sound.export(wav_path+wav_name, format="wav")
